bf5af5217a7415a0a1a778806eac21d31215b2bf
TakumiHANAUE <40872702+TakumiHANAUE@users.noreply.github.com>
2020-12-05 13:25:25 +0000

    day1 part1 program

---
+++ day1/day1_part1.nim
@@ -0,0 +1,25 @@
+import strutils
+import algorithm
+
+const SUM : int = 2020
+var report : seq[int]
+
+var inputFile: File
+inputFile = open("./day1input.txt", fmRead)
+
+for line in inputFile.lines:
+    report.add(line.parseInt())
+sort(report, Ascending)
+
+for i in 0..report.len:
+    var counterpart: int = SUM - report[i]
+    var counterpartIndex : int
+    counterpartIndex = binarySearch(report, counterpart)
+    # found counterpart
+    if counterpartIndex != -1:
+        echo "Two entries ", report[i], ", ", counterpart
+        echo "Multiplication result : ", (report[i] * counterpart)
+        break
+
+
+
